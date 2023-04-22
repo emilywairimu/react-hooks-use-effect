c77f259db34ca187169d8daf7ea8ac07b44fe973
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-04-22 17:18:31 -0500

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+// import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+  
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+
+  return(
+    <div>
+      <button onClick={()=>setCount
+      (count + 1)}>You Clicked Me {count} times</button>
+      <input
+       type="text" 
+       placeholder="Type away..."
+        value= {text}
+       onChange={(e)=>setText(e.target.value)}
+       />
+       {/* <DogPics /> */}
+    </div>
+    
+    
+  )
+  
 }
 
 export default App;
